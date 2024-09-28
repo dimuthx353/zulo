5c3bdf7a78a0dbab7565470eb852c2b5e180c938
dimuthx353 <dimuthx353@gmail.com>
2024-09-28 20:45:06 +0530

    add-carousel-for-men-women-pages

--- pages/men.php
+++ pages/men.php
@@ -35,7 +35,7 @@
            crossorigin="anonymous" />
    </head>
 
-   <body>
+   <body class="bg-dark">
 
        <div class="container-fluid m-0 p-0 full-body d-flex flex-column">
            <!-- navigation  -->
@@ -46,6 +46,33 @@
            </header>
        </div>
 
+       <div id="carouselExampleIndicators" class="carousel slide mb-5 carousel-slider">
+           <div class="carousel-indicators">
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+           </div>
+
+           <div class="carousel-inner h-100">
+               <div class="carousel-item active">
+                   <img src="../assets/img/menpageslide03.jpg" class="d-block w-100" alt="...">
+               </div>
+               <div class="carousel-item">
+                   <img src="../assets/img/menpageslide02.jpg" class="d-block w-100" alt="...">
+               </div>
+               <div class="carousel-item">
+                   <img src="../assets/img/menpageslide01.jpg" class="d-block w-100" alt="...">
+               </div>
+           </div>
+           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+           </button>
+           <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+           </button>
+       </div>
 
        <div class="container d-flex product-section">
            <div class="col-3 border-red">
