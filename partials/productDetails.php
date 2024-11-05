c9b29ef813e41e4b08e1469c055fd848a33ac9ad
dimuthx353 <dimuthx353@gmail.com>
2024-11-05 19:48:35 +0530

    correct installment price

--- partials/productDetails.php
+++ partials/productDetails.php
@@ -142,7 +142,7 @@ if (isset($_SESSION["user_id"])) {
         </small>
 
         <h1 class="mt-3 text-danger">Rs <?php echo $product["price"] ?></h1>
-        <small class="text-muted">or 3 installments of $16.66</small>
+        <small class="text-muted">or 3 installments of <?php echo $product["price"] / 3, ".00 LKR" ?></small>
 
         <p class="mt-4 fw-bold">Style: Casual</p>
         <p class="fw-bold">Size:</p>
