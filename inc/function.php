05315ad4c2306c48ed0ac930566b858dfe014e0e
SandaminiBandara <imasha18bandara@gmail.com>
2024-11-06 15:56:39 +0530

    create fun emptyinput

--- inc/function.php
+++ inc/function.php
@@ -6,3 +6,16 @@ function stmtFailed($stmt)
         exit();
     }
 }
+
+function emptyInputSignup($fName, $lName, $email, $pwd, $phoneNum, $streetAddress, $city, $province, $zipCode)
+{
+    $isEmpty = null;
+
+    if (empty($fName) || empty($lName) || empty($email) || empty($pwd) || empty($phoneNum) || empty($streetAddress) || empty($city) || empty($province) || empty($zipCode)) {
+        $isEmpty = true;
+    } else {
+        $isEmpty = false;
+    }
+
+    return $isEmpty;
+}
