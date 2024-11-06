d94e64f577415fd3659175e8e2864ff169a27bab
imashatharushi <imashatharushi786@gmail.com>
2024-11-06 19:30:30 +0530

    add men product

---
+++ pages/men.php
@@ -0,0 +1,78 @@
+<!doctype html>
+   <html lang="en">
+
+   <head>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <title>Mens - Zulo</title>
+       <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
+
+       <!-- Bootstrap CDN -->
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+       <!-- External CSS  -->
+       <link rel="stylesheet" href="../assets/css/addCategories.min.css">
+       <link rel="stylesheet" href="../assets/css/women.min.css">
+       <link rel="stylesheet" href="../assets/css/reset.min.css">
+       <link rel="stylesheet" href="../assets/css/nav.min.css">
+       <link rel="stylesheet" href="./assets/css/footer.min.css">
+
+       <!-- Font Awesome CDN  -->
+       <link
+           rel="stylesheet"
+           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+           integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+           crossorigin="anonymous" />
+   </head>
+   <body>
+
+<div class="container-fluid m-0 p-0 full-body d-flex flex-column">
+    <!-- Navigation  -->
+    <header>
+       
+    </header>
+</div>
+
+<div id="carouselExampleIndicators" class="carousel slide mb-5 carousel-slider">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
+    <div class="carousel-inner h-100">
+        <div class="carousel-item active">
+            <p>
+            <h1>Men</h1>
+            </p>
+            <img src="../assets/img/menpageslide03.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <p>
+            <h1>Men</h1>
+            </p>
+            <img src="../assets/img/menpageslide02.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <p>
+            <h1>Men</h1>
+            </p>
+            <img src="../assets/img/menpageslide01.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<div class="container d-flex product-section">
+    <div class="col-3 border-red">
+        <h1>filer options</h1>
+    </div>
+    <div class="col-9 d-flex flex-wrap gap-4 border-red" id="men">
+        <h1 class="text-center w-100">Men</h1>
