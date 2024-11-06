cf238f435ed7a5bcfe2c9735c4962b3d1c27d139
imashatharushi <imashatharushi786@gmail.com>
2024-11-06 13:06:26 +0530

    complete product card

--- partials/productCard.php
+++ partials/productCard.php
@@ -3,6 +3,7 @@
 $heartClass = "bi bi-heart";
 
 
+
 if (isset($_SESSION["user_id"])) {
     // Assuming you already have the user_id and product_id
     $user_id = $_SESSION['user_id']; // Example: get from session
@@ -32,38 +33,51 @@ if (isset($_SESSION["user_id"])) {
 }
 ?>
 
+
 <div class='card shadow-sm' style='width: 18rem; margin: 0 auto'>
-    <a href='../../zulo/partials/productDetails.php?product_id=' class="text-decoration-none  d-block">
+    <a href='../../zulo/partials/productDetails.php?product_id=<?php echo $productId ?>' class="text-decoration-none  d-block">
         <div>
             <img
-                src=""
+                src="<?php echo $imgPathLow ?>"
                 data-sizes="auto"
-                data-src=""
+                data-src="<?php echo $imgPathOrg ?>"
                 class="lazyload blur-up d-block w-100"
                 alt="Example Image 3" />
         </div>
         <div class='card-body'>
             <!-- Product Title -->
             <h6 class='card-title text-center fw-bold text-uppercase mb-4'>
-               
+                <?php echo $productTitle ?>
             </h6>
     </a>
     <!-- Price and Icons Section -->
     <div class='bg-light p-3 rounded shadow-sm'>
         <div
             class='d-flex justify-content-between align-items-center mb-3'>
-            <span class='h5 fw-bold text-danger'> Rs. 
+            <span class='h5 fw-bold text-danger'> Rs. <?php echo $productPrice ?>
             </span>
             <div class='d-flex gap-3 align-items-center'>
                 <i class='bi bi-bag fs-5 text-dark'></i>
-             
+                <?php
+                if ($heartClass == "bi bi-heart") {
+                    echo
+                    "
+        <button class='btn' > <i class='bi bi-heart fs-5 text-danger' onclick='addToWishlist(event," . $product_id . ")'   data-product_id='" . $productId . "'></i></button>
+        ";
+                } else if ($heartClass == "bi bi-heart-fill") {
+                    echo
+                    "
+        <button class='btn'> <i class='bi bi-heart-fill fs-5 text-danger' onclick='addToWishlist(event, " . $product_id . ")' data-product_id='" . $productId . "'></i></button>
+      ";
+                }
+                ?>
 
 
             </div>
         </div>
         <!-- Buy Now Buttons -->
         <div class='d-flex justify-content-center gap-2'>
-            <button class='btn btn-outline-danger' onclick="addToCart()">Add to Cart</button>
+            <button class='btn btn-outline-danger' onclick="addToCart(<?php echo $product_id ?>)">Add to Cart</button>
             <button class='btn btn-danger text-white'>Buy Now</button>
         </div>
     </div>
