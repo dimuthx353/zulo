529c567863a04aaf8addbeee248041f607d199c0
imashatharushi <imashatharushi786@gmail.com>
2024-11-06 13:03:18 +0530

    product card added

---
+++ partials/productCard.php
@@ -0,0 +1,38 @@
+<div class='card shadow-sm' style='width: 18rem; margin: 0 auto'>
+    <a href='../../zulo/partials/productDetails.php?product_id=' class="text-decoration-none  d-block">
+        <div>
+            <img
+                src=""
+                data-sizes="auto"
+                data-src=""
+                class="lazyload blur-up d-block w-100"
+                alt="Example Image 3" />
+        </div>
+        <div class='card-body'>
+            <!-- Product Title -->
+            <h6 class='card-title text-center fw-bold text-uppercase mb-4'>
+               
+            </h6>
+    </a>
+    <!-- Price and Icons Section -->
+    <div class='bg-light p-3 rounded shadow-sm'>
+        <div
+            class='d-flex justify-content-between align-items-center mb-3'>
+            <span class='h5 fw-bold text-danger'> Rs. 
+            </span>
+            <div class='d-flex gap-3 align-items-center'>
+                <i class='bi bi-bag fs-5 text-dark'></i>
+             
+
+
+            </div>
+        </div>
+        <!-- Buy Now Buttons -->
+        <div class='d-flex justify-content-center gap-2'>
+            <button class='btn btn-outline-danger' onclick="addToCart()">Add to Cart</button>
+            <button class='btn btn-danger text-white'>Buy Now</button>
+        </div>
+    </div>
+</div>
+
+</div>
