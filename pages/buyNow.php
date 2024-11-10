22a2cfbe6bf5b9c232b1da95eee902510a77f6ca
dimuthx353 <dimuthx353@gmail.com>
2024-11-10 11:22:39 +0530

    add correct image path to the footer in buy now page

--- pages/buyNow.php
+++ pages/buyNow.php
@@ -30,11 +30,19 @@ if (isset($_GET["product_id"])) {
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- Font Awesome CDN  -->
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" />
+
     <!-- External CSS  -->
     <!-- <link rel="stylesheet" href="../assets/css/cart.min.css"> -->
     <link rel="stylesheet" href="../assets/css/reset.min.css">
     <link rel="stylesheet" href="../assets/css/nav.min.css">
     <link rel="stylesheet" href="../assets/css/footer.min.css">
+    <link rel="stylesheet" href="../assets/css/index.min.css">
 </head>
 
 <body>
@@ -164,6 +172,8 @@ if (isset($_GET["product_id"])) {
     </div>
     </div>
     <?php
+    $imgPathForFooter = "../assets/img/";
+
     include_once "../partials/footer.php"
     ?>
 
