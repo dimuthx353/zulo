53a5dd74837b3f00271f6dac79c0e5efdf69c5d6
Maduka Karunarathna <madukakarunarathna8@gmail.com>
2024-11-10 16:54:01 +0530

    link logo to cart page

---
+++ pages/cart.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart - Zulo</title>
+    <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
+</head>
+<body>
+    
+</body>
+</html>
