4ccb5f15a77c8b9b61aece4c28c27da66f78c5c4
Maduka Karunarathna <madukakarunarathna8@gmail.com>
2024-11-10 16:54:48 +0530

    link css and bootstrap

--- pages/cart.php
+++ pages/cart.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cart - Zulo</title>
     <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- External CSS  -->
+    <link rel="stylesheet" href="../assets/css/cart.min.css">
+    <link rel="stylesheet" href="../assets/css/reset.min.css">
 </head>
 <body>
     
