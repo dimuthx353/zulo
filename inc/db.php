77a3cb74ca6e4263d058a2515a24812e1f99709c
dimuthx353 <dimuthx353@gmail.com>
2024-11-10 14:56:19 +0530

    fix small issue

--- inc/db.php
+++ inc/db.php
@@ -6,7 +6,7 @@ $username = "root";
 $password = "1234";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=zulo", $username, $password);
+    $conn = new PDO("mysql:host=$serverName;dbname=zulo", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
