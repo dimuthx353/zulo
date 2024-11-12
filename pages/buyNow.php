77f3a4bb79ed56610b943a571a084eee643c7ec4
Maduka Karunarathna <madukakarunarathna8@gmail.com>
2024-11-12 13:57:55 +0530

    link css and bootstrap

---
+++ pages/buyNow.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buy Now - Zulo</title>
+    <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
+
+     <!-- Bootstrap CDN -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<!-- Font Awesome CDN  -->
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+    crossorigin="anonymous" />
+
+<!-- External CSS  -->
+<!-- <link rel="stylesheet" href="../assets/css/cart.min.css"> -->
+<link rel="stylesheet" href="../assets/css/reset.min.css">
+<link rel="stylesheet" href="../assets/css/nav.min.css">
+<link rel="stylesheet" href="../assets/css/footer.min.css">
+<link rel="stylesheet" href="../assets/css/index.min.css">
+</head>
+
+<body>
+    
+</body>
+</html>
