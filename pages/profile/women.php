cb5bb5082d5778b1ad8133da53ebcaf0d0720a13
imashatharushi <imashatharushi786@gmail.com>
2024-11-12 13:16:49 +0530

    add image and filter options

--- pages/profile/women.php
+++ pages/profile/women.php
@@ -25,3 +25,54 @@
            integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
            crossorigin="anonymous" />
    </head>
+
+   <body>
+
+<div class="container-fluid m-0 p-0 full-body d-flex flex-column">
+    <!-- Navigation  -->
+    
+       </div>
+
+       <div id="carouselExampleIndicators" class="carousel slide mb-5 carousel-slider">
+           <div class="carousel-indicators">
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+           </div>
+
+           <div class="carousel-inner h-100">
+               <div class="carousel-item active">
+                   <img src="../assets/img/womenpageslide03.jpg" class="d-block w-100" alt="...">
+                   <p style="font-family: Playfair Display;">
+                       Explore New Trends
+                   </p>
+               </div>
+               <div class="carousel-item">
+                   <img src="../assets/img/womenpageslide02.jpg" class="d-block w-100" alt="...">
+                   <p style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
+                       Elevate Your Style
+                   </p>
+               </div>
+               <div class="carousel-item">
+                   <img src="../assets/img/womenpageslide01.jpg" class="d-block w-100" alt="...">
+                   <p style="font-family: Raleway;">
+                       Unleash Your Inner Fashionista
+                   </p>
+               </div>
+           </div>
+           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+           </button>
+           <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+           </button>
+       </div>
+
+       <div class="container d-flex product-section">
+           <div class="col-3 border-red">
+               <h1>filer options</h1>
+           </div>
+
+           
