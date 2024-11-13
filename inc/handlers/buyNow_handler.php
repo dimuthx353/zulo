faf79acf1c668f9b4526c6682a2a67808c54cfcf
Maduka Karunarathna <madukakarunarathna8@gmail.com>
2024-11-13 22:20:48 +0530

    handles the creation of a new order

--- inc/handlers/buyNow_handler.php
+++ inc/handlers/buyNow_handler.php
@@ -33,3 +33,30 @@ if (isset($_GET['product_id'])) {
     $product_id = $_GET['product_id'];
     // echo "product id: " . $product_id;
 }
+
+
+if ($user && $product_id) {
+    echo "hello";
+
+    $total = $_POST['productPrice'];
+    $paymentStatus = $_POST["paymentMethod"];
+    $shippingAddress = $_POST["name"];
+    $qty = 1;
+
+
+
+    $sql = "INSERT INTO orders (user_id, total_amount,shipping_address, payment_status, qty, product_id) 
+            VALUES (:user_id, :total_amount, :shipping_address, :payment_status, :qty, :product_id)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':user_id', $userID, PDO::PARAM_INT);
+    $stmt->bindParam(':total_amount', $total, PDO::PARAM_STR);
+    $stmt->bindParam(':payment_status', $paymentStatus, PDO::PARAM_STR);
+    $stmt->bindParam(':shipping_address', $shippingAddress, PDO::PARAM_STR);
+    $stmt->bindParam(':qty', $qty, PDO::PARAM_INT);  // Bind qty as integer
+    $stmt->bindParam(':product_id', $product_id, PDO::PARAM_INT);  // Bind product_id as integer
+
+    $stmt->execute();
+
+    header("Location: ../../../../../zulo/pages/orderSuccess.php?order_id=" . $conn->lastInsertId());
+}
