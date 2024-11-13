e1ab4d7a9d7cc0f58d9d2e927414233c5742738b
Maduka Karunarathna <madukakarunarathna8@gmail.com>
2024-11-13 22:58:55 +0530

    create a card for odersuccess

---
+++ pages/orderSuccess.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+   <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="card">
+        <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+            <i class="checkmark">✓</i>
+        </div>
+        <h1>Success</h1>
+        <p>We received your purchase request;<br /> we'll be in touch shortly!</p>
+        <p style="margin-top:10px ;">Your Order Id: <b><?php echo $orderId ?></b></p>
+
+        <a style="padding: 5px;margin:30px 0px;display:inline-block; background-color: #4CAF50;color: white;text-decoration: none" href="../../zulo/index.php">Click Here to Return Home</a>
+    </div>
+    
+</body>
+
+</html>
