2f121fbd558836525a488d0f15d33050fdbbf7f8
dimuthx353 <dimuthx353@gmail.com>
2024-11-14 14:57:02 +0530

    add sweetAlert for update product details in admin

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -295,6 +295,8 @@ $Products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js' integrity='sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==' crossorigin='anonymous'></script>
     <!-- Bootstrap CDN  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- SweetAlert2 CDN  -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- External JS  -->
     <script src="../../assets/js/admin.js"></script>
     <script src="../../assets/js/updateProduct.js"></script>
