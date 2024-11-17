e5bff58ea55fc44e454340c87d4224b73047fef6
dimuthx353 <dimuthx353@gmail.com>
2024-11-17 15:46:57 +0530

    add filter option for women page

--- inc/handlers/filter_handler.php
+++ inc/handlers/filter_handler.php
@@ -4,11 +4,12 @@ include_once "../db.php";
 
 if (isset($_GET["sort"])) {
     $sort = $_GET["sort"];
+    $page = $_GET["page"];
 
     if ($sort == "ASC") {
-        $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products ORDER BY price ASC";
+        $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products WHERE category_id = $page ORDER BY price ASC";
     } else if ($sort == "DESC") {
-        $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products ORDER BY price DESC";
+        $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products WHERE category_id = $page ORDER BY price DESC";
     }
 } else if (isset($_GET["category"])) {
     $category = $_GET["category"];
@@ -19,6 +20,12 @@ if (isset($_GET["sort"])) {
         $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products WHERE subCategory = 'Shirts M'";
     } else if ($category == "casualShirts") {
         $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products WHERE subCategory = 'casualShirts M'";
+    } elseif ($category == "Denims") {
+        $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products WHERE subCategory = 'Denims W'";
+    } elseif ($category == "T-Shirts") {
+        $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products WHERE subCategory = 'Tshirts W'";
+    } else if ($category == "TopsW") {
+        $sql = "SELECT product_id, product_name, description, price, stock_quantity, image_url, sku FROM products WHERE subCategory = 'Tops W'";
     }
 }
 
